a84470ab1d58ea2d15a12164bc94eb0818394fb0
Janusz Sidor <sidor@innen.pl>
2014-08-09 14:05:55 +0200

    added jms

---
+++ src/main/groovy/com/ofg/microservice/result/SenderService.groovy
@@ -0,0 +1,40 @@
+package com.ofg.microservice.result
+
+import com.sun.media.sound.ModelDestination
+import groovy.transform.CompileStatic
+import groovy.util.logging.Slf4j
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.jms.core.JmsTemplate
+import org.springframework.jms.core.MessageCreator
+import org.springframework.stereotype.Service
+
+import javax.jms.Destination
+import javax.jms.JMSException
+import javax.jms.Message
+import javax.jms.Session
+import javax.jms.TextMessage
+
+/**
+ * Created by januszsidor on 09/08/14.
+ */
+@CompileStatic
+@Slf4j
+@Service
+class SenderService {
+    @Autowired
+    JmsTemplate template;
+    final static String PAIR_ID = "PAIR_ID"
+    final static String TOPIC_NAME = "facebook";
+
+    public void post(String data, String pairId){
+       //Destination destination = new Destination()
+       template.send(TOPIC_NAME, new MessageCreator() {
+           public Message createMessage(Session session) throws JMSException {
+               TextMessage message = session.createTextMessage(data);
+               message.setIntProperty(PAIR_ID, pairId);
+               log.info("Sending for pairID {} message: {}",  pairId, data);
+               return message;
+           }
+       })
+    }
+}
---
+++ src/test/groovy/com/ofg/microservice/result/SenderServiceTest.groovy
@@ -0,0 +1,12 @@
+package com.ofg.microservice.result
+
+/**
+ * Created by januszsidor on 09/08/14.
+ */
+//@RuntWith()
+//@SprinTes
+class SenderServiceTest extends GroovyTestCase {
+    void testPost() {
+
+    }
+}
