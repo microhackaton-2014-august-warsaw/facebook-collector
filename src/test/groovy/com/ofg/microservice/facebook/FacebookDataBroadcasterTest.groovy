45122e4c5f016e541b124a2fc18fba6a92876468
Darek Kaczyński <kaczynskid@gmail.com>
2014-08-09 15:01:37 +0200

    Corrected failing test

--- src/test/groovy/com/ofg/microservice/facebook/FacebookDataBroadcasterTest.groovy
+++ src/test/groovy/com/ofg/microservice/facebook/FacebookDataBroadcasterTest.groovy
@@ -1,22 +1,14 @@
 package com.ofg.microservice.facebook
 
-import com.ofg.microservice.Application
-import org.junit.Test
-import org.junit.runner.RunWith
+import com.ofg.base.MicroserviceMvcWiremockSpec
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.SpringApplicationContextLoader
-import org.springframework.test.context.ContextConfiguration
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = Application.class, loader = SpringApplicationContextLoader.class)
-class FacebookDataBroadcasterTest extends GroovyTestCase {
+class FacebookDataBroadcasterTest extends MicroserviceMvcWiremockSpec {
 
     @Autowired
     FacebookDataBroadcaster broadcaster
 
-    @Test
-    void testPost() {
+    def 'testPost'() {
         broadcaster.broadcast(new FacebookData(), 111L);
     }
 }
